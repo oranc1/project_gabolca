ee0576b630e3b991a5376218018d478a891868b7
ITWILL\user <lcjnys@naver.com>
2023-06-27 17:09:16 +0900

    커밋

--- GaBolCar/src/main/java/com/itwillbs/project_gabolcar/controller/CarItemController.java
+++ GaBolCar/src/main/java/com/itwillbs/project_gabolcar/controller/CarItemController.java
@@ -865,31 +865,31 @@ public class CarItemController {
 			
 			Map<String,Object> map = null;
 			
-			//글쓰기 제어 : admin이 아닐 때 예약이 있을 때
-			if(sId == null || sId.length() == 0)
-			{
-				model.addAttribute("msg", "로그인해 주십시오."); // 로그인 안했을 때
-				model.addAttribute("targetURL", "login"); // 로그인 페이지로 이동
-				// 코드 재사용 원래는 실패지만 
-				// success_forward가 메시지를 띄우고 원하는 페이지로 이동하기 때문에 사용
-				return "inc/success_forward";
-			}
-			else
-			{
-				if(!sId.equals("admin@admin.com")) {
-					int isBoardWriter = carItemService.isBoardWriter(sId);
-					
-					if(isBoardWriter == 0 
-							|| resIdx < 0 
-							|| carItemService.isAlreadyWriteRev(resIdx)) {
-						model.addAttribute("msg", "권한이 없습니다!");
-						return "html/car_item/review/fail_back";
-					}
-				}
-			}
+//			//글쓰기 제어 : admin이 아닐 때 예약이 있을 때
+//			if(sId == null || sId.length() == 0)
+//			{
+//				model.addAttribute("msg", "로그인해 주십시오."); // 로그인 안했을 때
+//				model.addAttribute("targetURL", "login"); // 로그인 페이지로 이동
+//				// 코드 재사용 원래는 실패지만 
+//				// success_forward가 메시지를 띄우고 원하는 페이지로 이동하기 때문에 사용
+//				return "inc/success_forward";
+//			}
+//			else
+//			{
+//				if(!sId.equals("admin@admin.com")) {
+//					int isBoardWriter = carItemService.isBoardWriter(sId);
+//					
+//					if(isBoardWriter == 0 
+//							|| resIdx < 0 
+//							|| carItemService.isAlreadyWriteRev(resIdx)) {
+//						model.addAttribute("msg", "권한이 없습니다!");
+//						return "html/car_item/review/fail_back";
+//					}
+//				}
+//			}
 			
 			// 예약번호로 차량 정보 가져오기
-			map = carItemService.selectResNCarInfo(sId,resIdx);
+			map = carItemService.selectResNCarInfo(null,resIdx);
 			if(map == null) {
 				model.addAttribute("msg", "권한이 없거나 예약 정보를 가져오는중에 문제가 발생되었습니다!");
 				return "html/car_item/review/fail_back";
@@ -905,26 +905,26 @@ public class CarItemController {
 		public String reviewWritePro(HttpSession session, ReviewVO review, Model model, @RequestParam Map<String,Object> map,HttpServletRequest request) {
 			String sId = (String)session.getAttribute("sId");
 
-			//글쓰기 제어 : admin이 아닐 때 예약이 있을 때
-			if(sId == null || sId.length() == 0)
-			{
-				model.addAttribute("msg", "로그인해 주십시오."); // 로그인 안했을 때
-				model.addAttribute("targetURL", "login"); // 로그인 페이지로 이동
-				// 코드 재사용 원래는 실패지만 
-				// success_forward가 메시지를 띄우고 원하는 페이지로 이동하기 때문에 사용
-				return "inc/success_forward";
-			}
-			else
-			{
-				if(!sId.equals("admin@naver.com")) {
-					int isBoardWriter = carItemService.isBoardWriter(sId);
-					
-					if(isBoardWriter == 0 ) {
-						model.addAttribute("msg", "권한이 없습니다!");
-						return "html/car_item/review/fail_back";
-					}
-				}
-			}
+//			//글쓰기 제어 : admin이 아닐 때 예약이 있을 때
+//			if(sId == null || sId.length() == 0)
+//			{
+//				model.addAttribute("msg", "로그인해 주십시오."); // 로그인 안했을 때
+//				model.addAttribute("targetURL", "login"); // 로그인 페이지로 이동
+//				// 코드 재사용 원래는 실패지만 
+//				// success_forward가 메시지를 띄우고 원하는 페이지로 이동하기 때문에 사용
+//				return "inc/success_forward";
+//			}
+//			else
+//			{
+//				if(!sId.equals("admin@naver.com")) {
+//					int isBoardWriter = carItemService.isBoardWriter(sId);
+//					
+//					if(isBoardWriter == 0 ) {
+//						model.addAttribute("msg", "권한이 없습니다!");
+//						return "html/car_item/review/fail_back";
+//					}
+//				}
+//			}
 
 			
 			String uploadDir = "/resources/upload";
